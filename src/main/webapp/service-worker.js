0ed017d6cd416a03eaa3731099c41c6d670adb32
ASAMI Keisuke <syakeApps@icloud.com>
2022-08-06 02:41:10 +0900

    add file

---
+++ src/main/webapp/service-worker.js
@@ -0,0 +1,27 @@
+// キャッシュ名とキャッシュ対象の指定
+var CACHE_NAME = 'sprandoom2-caches';
+var urlsToCache = [
+  '/sprandoom2/'
+];
+
+// インストール処理
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(function(c) {
+        return c.addAll(urlsToCache);
+      })
+  );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then(function(res) {
+        return res ? res : fetch(e.request);
+      })
+  );
+});
